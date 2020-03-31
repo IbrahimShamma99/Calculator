0d05f055d71d3e2fabf3cfe821085f4bd61cb396
ibrahimshamma99 <i.abushammah@gmail.com>
2020-03-31 10:55:40 +0300

    update

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,9 +5,9 @@ const calc = () =>(
     <div className="container">
 
     <div className="row">
-    <button type="button" class="btn btn-outline-danger">Danger</button>
-    <button type="button" class="btn btn-outline-danger">Danger</button>
-    <button type="button" class="btn btn-outline-danger">Danger</button>
+    <button type="button" class="btn btn-outline-danger">!</button>
+    <button type="button" class="btn btn-outline-danger">2</button>
+    <button type="button" class="btn btn-outline-danger">3</button>
     </div>
 
     <div className="row">
